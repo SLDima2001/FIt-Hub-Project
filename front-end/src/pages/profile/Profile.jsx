ac99a69e4834b68b3a47631acb1a656f214256b1
Dimalsha Praveen <dimalshapraveen2001@gmail.com>
2025-05-06 23:02:11 +0530

    change

--- front-end/src/pages/profile/Profile.jsx
+++ front-end/src/pages/profile/Profile.jsx
@@ -12,7 +12,7 @@ import PlaceIcon from "@mui/icons-material/Place";
 import LanguageIcon from "@mui/icons-material/Language";
 import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import ProfilePosts from "../../components/posts/ProfilePosts"
+import Posts from "../../components/posts/Posts"
 import { DarkModeContext } from "./../../context/darkModeContext";
 import { useParams } from "react-router-dom";
 import React, { useState, useEffect } from "react";
@@ -110,7 +110,7 @@ const Profile = () => {
             <MoreVertIcon />
           </div>
         </div>
-      <ProfilePosts userName={userName}/>
+      <Posts userName={userName}/>
       </div>
     </div>
     </div>
